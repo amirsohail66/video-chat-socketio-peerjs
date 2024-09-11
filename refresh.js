66e52eb3e7fd83d77b91c40898af54ac03654f21
Md Amir Sohail <94072183+amirsohail66@users.noreply.github.com>
2024-09-11 16:52:12 +0530

    Update refresh.js

--- refresh.js
+++ refresh.js
@@ -1,5 +1,11 @@
 setInterval(() => {
-  fetch("https://audio-video-chat.onrender.com/");
-  fetch("https://foodmine-eakr.onrender.com/");
-  console.log(/called refresh/);
+  fetch("https://audio-video-chat.onrender.com/")
+    .then(response => console.log('Audio-video chat refreshed'))
+    .catch(error => console.error('Error refreshing audio-video chat:', error));
+
+  fetch("https://foodmine-eakr.onrender.com/")
+    .then(response => console.log('Foodmine refreshed'))
+    .catch(error => console.error('Error refreshing Foodmine:', error));
+
+  console.log('called refresh');
 }, 20000);
